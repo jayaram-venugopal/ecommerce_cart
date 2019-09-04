6204028b3eccc9d709f847b55d5321ee247acb07
Jayaram.V <jayaram.v@robosoftin.com>
2019-09-04 12:39:46 +0530

    Updated: Card discount test cases

--- spec/services/promotion/cart_discount_spec.rb
+++ spec/services/promotion/cart_discount_spec.rb
@@ -2,27 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Promotion::CartDiscount do
 
-  context 'calculate total order for prodcut A,B,C' do
+  it 'calculate total order for prodcut A,B,C' do
+    @order = create(:order)
     data = [{:product_id =>  1, :quantity => 1}, {:product_id =>  2, :quantity => 1}, {:product_id =>  3, :quantity => 1}]
-    let(:order) {build(:order, data.map { |p| build(:order_item, p) })}
-
-    subject(:calculate) do
-      Promotion::BasketDiscount.new(order).calculate 
-    end
+    order_items = data.map { |p| create(:order_item, :product_id => p[:product_id], :quantity => p[:quantity], :order_id => @order.id) }
+    calculate = Promotion::BasketDiscount.new(@order).calculate 
     
-    it { expect(calculate[0]).to eq(100.0) }
+    expect(calculate[1]).to match(100.0)
   end
 
-  context 'calculate total order for prodcut B,A,B,A,A' do
-    order = FactoryBot.build(:order)
-    data = [{:product_id =>  2, :quantity => 1}, {:product_id =>  1, :quantity => 1}, {:product_id =>  2, :quantity => 1}, {:product_id =>  1, :quantity => 1}, {:product_id =>  1, :quantity => 1}]
-    order_items = data.map { |p| FactoryBot.create(:order_item, p) } 
-
-    subject(:calculate) do
-      Promotion::BasketDiscount.new(order).calculate 
-    end
+  it 'calculate total order for prodcut B,A,B,A,A' do
+    @order = create(:order)
+    data = [{:product_id =>  2, :quantity => 2}, {:product_id =>  1, :quantity => 3}]
+    order_items = data.map { |p| create(:order_item, :product_id => p[:product_id], :quantity => p[:quantity], :order_id => @order.id) }
+    calculate = Promotion::BasketDiscount.new(@order).calculate 
     
-    it { expect(calculate[1]).to eq(110.0) }
+    expect(calculate[1]).to match(110.0)
+  end
+
+  it 'calculate total order for prodcut C, B, A, A, D, A, B' do
+    @order = create(:order)
+    data = [{:product_id =>  1, :quantity => 3}, {:product_id =>  2, :quantity => 2}, {:product_id =>  3, :quantity => 1}, {:product_id =>  4, :quantity => 1}]
+    order_items = data.map { |p| create(:order_item, :product_id => p[:product_id], :quantity => p[:quantity], :order_id => @order.id) }
+    calculate = Promotion::BasketDiscount.new(@order).calculate 
+    
+    expect(calculate[1]).to match(155.0)
+  end
+
+  it 'calculate total order for prodcut C, A, D, A , A' do
+    @order = create(:order)
+    data = [{:product_id =>  1, :quantity => 3}, {:product_id =>  3, :quantity => 1}, {:product_id =>  4, :quantity => 1}]
+    order_items = data.map { |p| create(:order_item, :product_id => p[:product_id], :quantity => p[:quantity], :order_id => @order.id) }
+    calculate = Promotion::BasketDiscount.new(@order).calculate 
+    
+    expect(calculate[1]).to match(140.0)
   end
   
 
